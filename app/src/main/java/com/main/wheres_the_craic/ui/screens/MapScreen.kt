21de0539dcf35aa8c2eef81a9463e567a607ed26
Kayan Gemingani <119429909+KayanGS@users.noreply.github.com>
2025-10-17 17:55:13 +0100

    Add google dependencies
    - Added google maps dependencies

--- app/src/main/java/com/main/wheres_the_craic/ui/screens/MapScreen.kt
+++ app/src/main/java/com/main/wheres_the_craic/ui/screens/MapScreen.kt
@@ -2,7 +2,13 @@ package com.main.where_s_the_craic.ui.screens
 
 import androidx.compose.runtime.Composable
 
+
+/**
+ * This Screen will be the main screen of the app, will show the map using google maps, and locate
+ * the pubs closer to the user and show in the map, clicking in a pub will redirect the user to the
+ * check-in screen
+ */
 @Composable
 fun MapScreen() {
-    //TO-DO
+
 }
