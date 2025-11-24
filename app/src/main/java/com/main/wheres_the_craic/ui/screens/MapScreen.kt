e9fb5f099a99d7281f68090d2d28d413a256339f
Kayan Gemingani <119429909+KayanGS@users.noreply.github.com>
2025-11-24 11:14:48 +0000

    feat: Integrate Places API for fetching nearby pubs
    - Added PlacesAPI.kt to fetch nearby pubs using Google Places API.
    - Updated MapScreen to display pub markers on the map based on API results.

--- app/src/main/java/com/main/wheres_the_craic/ui/screens/MapScreen.kt
+++ app/src/main/java/com/main/wheres_the_craic/ui/screens/MapScreen.kt
@@ -19,15 +19,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.libraries.places.api.Places
-import com.google.android.libraries.places.api.model.Place
-import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
-import com.google.android.libraries.places.api.net.PlacesClient
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.main.wheres_the_craic.R
-
+import com.main.wheres_the_craic.data.PlaceResult
+import com.main.wheres_the_craic.data.fetchNearbyPubs
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 /**
  * This Screen will be the main screen of the app, will show the map using google maps, and locate
@@ -43,14 +45,13 @@ fun MapScreen(onPubSelected: (String) -> Unit = {}) {
     var hasPermission by remember { mutableStateOf(false) }
     var userPosition by remember { mutableStateOf(LatLng(53.3498, -6.2603)) } // Dublin default
     var locationLoaded by remember { mutableStateOf(false) }
-    var pubs by remember { mutableStateOf<List<Place>>(emptyList()) }
+    var nearbyPubs by remember { mutableStateOf<List<PlaceResult>>(emptyList()) }
 
     // Ask for permission and handle if permission is granted or denied
     LocationPermission(
         onPermissionGranted = { hasPermission = true },
         onPermissionDenied = { hasPermission = false }
     )
-
 
     // After permission, fetch last location, temporarily Dublin
     LaunchedEffect(hasPermission) {
@@ -63,24 +64,6 @@ fun MapScreen(onPubSelected: (String) -> Unit = {}) {
                 context.getString(R.string.google_maps_key)
             )
         }
-        // Initialize the Places client
-        val placesClient: PlacesClient = Places.createClient(context)
-
-        val placesFields = listOf(
-            Place.Field.ID,
-            Place.Field.NAME,
-            Place.Field.LAT_LNG,
-            Place.Field.TYPES,
-            Place.Field.ADDRESS
-        )
-
-        // Create a FindCurrentPlaceRequest to fetch the current place
-        val request = FindCurrentPlaceRequest.newInstance(placesFields)
-
-        placesClient.findCurrentPlace(request).addOnSuccessListener { response ->
-            //TO-DO
-        }
-
 
         // Fetch last location
         val fused = LocationServices.getFusedLocationProviderClient(context) // get location client
@@ -98,6 +81,29 @@ fun MapScreen(onPubSelected: (String) -> Unit = {}) {
             }
     }
 
+    LaunchedEffect(locationLoaded) {
+        if (!locationLoaded) return@LaunchedEffect // if location not loaded, do nothing
+
+        val searchRadius = 95000 // 5km
+
+        try {
+            // Create a coroutine to fetch nearby pubs
+            val nearbyPubsResults = withContext(Dispatchers.IO) {
+                val apiKey = context.getString(R.string.google_maps_key) // Get api key
+
+                // Fetch nearby pubs
+                fetchNearbyPubs(
+                    userPosition.latitude,
+                    userPosition.longitude,
+                    apiKey,
+                    searchRadius
+                )
+            }
+            nearbyPubs = nearbyPubsResults
+        } catch (e: Exception) {
+            nearbyPubs = emptyList()
+        }
+    }
     // Create the camera position state, with a default position
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(userPosition, 14f)
@@ -127,6 +133,22 @@ fun MapScreen(onPubSelected: (String) -> Unit = {}) {
                     zoomControlsEnabled = true
                 )
             ) {
+                nearbyPubs.forEach { place ->
+                    Marker(
+                        state = MarkerState(
+                            position = LatLng(
+                                place.pubLatitude,
+                                place.pubLongitude
+                            )
+                        ),
+                        title = place.pubName,
+                        onClick = {
+                            onPubSelected(place.pubId ?: "")
+                            true
+                        }
+                    )
+
+                }
             }
         }
     }
